f35fd0f3a805c46405603352b15e4e1c89599a9c
Victor Ortiz <victorandresortiz@gmail.com>
2024-08-13 18:56:24 -0600

    Agregacion de controlador y clase restaurante y funcionalidad con base de datos

---
+++ src/main/java/pojos/Restaurante.java
@@ -0,0 +1,47 @@
+package pojos;
+
+public class Restaurante {
+
+    private String nombre;
+    private String ciudad;
+    private String descripcion;
+    private String id;
+
+    public Restaurante(String nombre, String ciudad, String descripcion) {
+        this.nombre = nombre;
+        this.ciudad = ciudad;
+        this.descripcion = descripcion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
